5189a3786c6bcc95d25ee23671c444354558e74b
Alex McLain <alex@alexmclain.com>
2018-06-08 12:42:47 -0700

    Add TLOG integration test.

--- integration/spec/integration_spec.exs
+++ integration/spec/integration_spec.exs
@@ -36,4 +36,52 @@ defmodule Integration.Spec do
       value     |> should(eq "72.1")
     end
   end
+
+  describe "TLOG" do
+    let :connection do
+      {:ok, conn} = Jylis.start_link("jylis://localhost")
+      conn
+    end
+
+    finally do: connection() |> Jylis.stop
+
+    specify do
+      {:ok, _} = connection() |> Jylis.TLOG.ins("temperature", 68, 1528238310)
+      {:ok, _} = connection() |> Jylis.TLOG.ins("temperature", 70, 1528238320)
+      {:ok, _} = connection() |> Jylis.TLOG.ins("temperature", 73, 1528238330)
+
+      {:ok, values} = connection() |> Jylis.TLOG.get("temperature")
+      values |> should(eq [
+        {"73", 1528238330},
+        {"70", 1528238320},
+        {"68", 1528238310},
+      ])
+
+      {:ok, size} = connection() |> Jylis.TLOG.size("temperature")
+      size |> should(eq 3)
+
+      {:ok, _} = connection() |> Jylis.TLOG.trimat("temperature", 1528238320)
+
+      {:ok, cutoff} = connection() |> Jylis.TLOG.cutoff("temperature")
+      cutoff |> should(eq 1528238320)
+
+      {:ok, values} = connection() |> Jylis.TLOG.get("temperature")
+      values |> should(eq [
+        {"73", 1528238330},
+        {"70", 1528238320},
+      ])
+
+      {:ok, _} = connection() |> Jylis.TLOG.trim("temperature", 1)
+
+      {:ok, values} = connection() |> Jylis.TLOG.get("temperature")
+      values |> should(eq [
+        {"73", 1528238330},
+      ])
+
+      {:ok, _} = connection() |> Jylis.TLOG.clr("temperature")
+
+      {:ok, values} = connection() |> Jylis.TLOG.get("temperature")
+      values |> should(eq [])
+    end
+  end
 end
