52f8f47bf03c20b9440c9e94fd0a87b8fc90bec5
Alex McLain <alex@alexmclain.com>
2018-08-08 13:00:34 -0700

    Increment version to 0.3.0.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jylis.MixProject do
   def project do
     [
       app:               :jylis_ex,
-      version:           "0.2.0",
+      version:           "0.3.0",
       elixir:            "~> 1.6",
       package:           package(),
       deps:              deps(),
